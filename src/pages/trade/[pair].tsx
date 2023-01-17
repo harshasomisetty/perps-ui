8ff9385e9e2ebada9cd79013c1d7bf7a7cd5b0bc
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-01-16 22:31:30 -0500

    Added API to get price values

--- src/pages/trade/[pair].tsx
+++ src/pages/trade/[pair].tsx
@@ -1,15 +1,46 @@
 import DailyStats from "@/components/DailyStats";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Page() {
   const router = useRouter();
   const { pair } = router.query;
 
+  let pairNames = { SOL: "solana" };
+
   const [price, setPrice] = useState(0);
   const [change, setChange] = useState(0);
   const [high, setHigh] = useState(0);
   const [low, setLow] = useState(0);
+
+  // TODO - get price highs and lows
+  const getPriceData = async () => {
+    console.log("pair", pair);
+
+    let names = pair.split("-");
+    console.log("names", names);
+    console.log("pairNames", pairNames[names[0]]);
+
+    let resp = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=" +
+        pairNames[names[0]] +
+        "&vs_currencies=" +
+        names[1] +
+        "&include_24hr_vol=true&include_24hr_change=true"
+    );
+    let data = resp.data[Object.keys(resp.data)[0]];
+    console.log("data", data);
+    setPrice(data.usd);
+    setChange(data.usd_24h_change.toFixed(2));
+  };
+
+  useEffect(() => {
+    if (pair && price == 0) {
+      console.log("pair", pair);
+      getPriceData();
+    }
+  }, [pair]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="flex flex-row">
