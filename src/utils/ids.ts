5428203257c4aefe734316274caeb1d1ff161a94
theDefiBat <theDefiBat@gmail.com>
2023-02-27 09:43:36 +0530

    added ids.ts

---
+++ src/utils/ids.ts
@@ -0,0 +1,89 @@
+import { Cluster, PublicKey } from '@solana/web3.js';
+import ids from './ids.json';
+
+export class Id {
+  constructor(
+    public cluster: Cluster,
+    public poolName: string,
+    public poolAddress: string,
+    public lpTokenMint: string,
+    // public perpMarketAccountKey: string,
+    // public multisigAccountKey: string,
+    // public transferAuthorityAccountKey: string,
+
+    public tokenInfos: { 
+      symbol: string;
+      mintKey: string;
+      decimals: number;
+      isStable: boolean;
+      }[],
+
+    public custodies: {
+      custodyAccount : string;
+      tokenAccount : string;
+      symbol: string;
+      mintKey: string;
+      decimals: number;
+      isStable : boolean,
+      oracleAddress : string;
+    }[],
+  ) {}
+
+  public getAllTokenMints(): PublicKey[] {
+    return Array.from(
+      this.tokenInfos.map((token) => new PublicKey(token.mintKey)),
+    );
+  }
+
+  public getNonStableTokens(): PublicKey[] {
+    return Array.from(
+      this.tokenInfos
+        .filter((token) => !token.isStable)
+        .map((token) => new PublicKey(token.mintKey)),
+    );
+  }
+ 
+  public getAllCustodies(): PublicKey[] {
+    return Array.from(
+      this.custodies.map((custody) => new PublicKey(custody.custodyAccount)),
+    );
+  }
+
+  public getNonStableCustodies(): PublicKey[] {
+    return Array.from(
+      this.custodies
+        .filter((custody) => !custody.isStable)
+        .map((custody) => new PublicKey(custody.custodyAccount)),
+    );
+  }
+
+  static fromIdsByName(name: string): Id {
+    const poolConfig = ids.pools.find((id) => id['poolName'] === name);
+    if (!poolConfig) throw new Error(`No pool config ${name} found in Ids!`);
+    return new Id(
+      poolConfig.cluster as Cluster,
+      poolConfig.poolName,
+      poolConfig.poolAddress,
+      poolConfig.lpTokenMint,
+      poolConfig['tokens'],
+      poolConfig['custodies'],
+    );
+  }
+
+  static fromIdsByPk(poolPk: PublicKey): Id {
+    const poolConfig = ids.pools.find(
+      (id) => id['poolAddress'] === poolPk.toString(),
+    );
+    if (!poolConfig)
+      throw new Error(`No pool config ${poolPk.toString()} found in Ids!`);
+      return new Id(
+        poolConfig.cluster as Cluster,
+        poolConfig.poolName,
+        poolConfig.poolAddress,
+        poolConfig.lpTokenMint,
+        poolConfig['tokens'],
+        poolConfig['custodies'],
+      );
+  }
+
+}
